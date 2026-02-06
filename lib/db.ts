beedaa106b87c99595f05123a77eb08388290428
bmarian <33843056+bmarian@users.noreply.github.com.>
2026-02-06 15:02:24 +0200

    WIP: api improvements

--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,9 @@
 "use server";
 
-import { Client, createClient} from "@libsql/client";
+import {createClient} from "@libsql/client";
 
 const db = createClient({url: "file:local.db"});
 
 export async function getDb() {
   return db;
-}
-
-export async function getFromDbWithFallback(dbQueryFn, fallback) {
-  const db = await getDb();
-
-  try {
-    return await dbQueryFn(db);
-  } catch (error) {
-    console.warn("Could not find data in db, using fallback", error);
-    return await fallback(db);
-  }
 }
