ad572ea6762d3324c580b614ad6ac9e602d52495
bmarian <33843056+bmarian@users.noreply.github.com.>
2026-02-19 19:52:31 +0200

    Standardize JSON and DB field naming conventions in `vehicle.go` for consistency

--- backend/models/vehicle.go
+++ backend/models/vehicle.go
@@ -10,10 +10,10 @@ type Vehicle struct {
 	Latitude             sql.NullFloat64 `json:"latitude,omitempty" db:"latitude"`
 	Longitude            sql.NullFloat64 `json:"longitude,omitempty" db:"longitude"`
 	Timestamp            string          `json:"timestamp" db:"timestamp"`
-	VehicleType          int             `json:"vehicleType" db:"vehicle_type"`
-	BikeAccessible       sql.NullString  `json:"bikeAccessible,omitempty" db:"bike_accessible"`
-	WheelchairAccessible sql.NullString  `json:"wheelchairAccessible,omitempty" db:"wheelchair_accessible"`
+	VehicleType          int             `json:"vehicle_type" db:"vehicle_type"`
+	BikeAccessible       sql.NullString  `json:"bike_accessible,omitempty" db:"bike_accessible"`
+	WheelchairAccessible sql.NullString  `json:"wheelchair_accessible,omitempty" db:"wheelchair_accessible"`
 	Speed                sql.NullFloat64 `json:"speed,omitempty" db:"speed"`
-	RouteID              sql.NullInt64   `json:"routeId,omitempty" db:"route_id"`
-	TripID               sql.NullString  `json:"tripId,omitempty" db:"trip_id"`
+	RouteID              sql.NullInt64   `json:"route_id,omitempty" db:"route_id"`
+	TripID               sql.NullString  `json:"trip_id,omitempty" db:"trip_id"`
 }
