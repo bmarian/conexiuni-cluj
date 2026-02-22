6696dc4c7fc6eff4fef79ba56f31968f923dddeb
bmarian <33843056+bmarian@users.noreply.github.com.>
2026-02-22 12:17:16 +0200

    Add detailed inline comments in stop_time handler for trip, stop, route, and shape data structures

--- backend/handlers/stop_time.go
+++ backend/handlers/stop_time.go
@@ -37,3 +37,74 @@ func requestStopTimes(tranzyClient *tranzy.Client) ([]models.StopTime, error) {
 	}
 	return out, nil
 }
+
+/*
+All data for trip_id: 1_0
+
+stop_times by trip_id 1_0:
+  {
+    "trip_id": "1_0",
+    "stop_id": 1,
+    "stop_sequence": 0
+  },
+  {
+    "trip_id": "1_0",
+    "stop_id": 2,
+    "stop_sequence": 1
+  },
+  ...
+
+trip of trip_id:
+  {
+    "route_id": 1,
+    "trip_id": "1_0",
+    "trip_headsign": "P-ta 1 Mai Sos",
+    "direction_id": 0,
+    "block_id": 1,
+    "shape_id": "1_0"
+  }
+
+stop of stop_id:
+  {
+    "stop_id": 1,
+    "stop_name": "Disp. Clăbucet",
+    "stop_lat": 46.75144,
+    "stop_lon": 23.54292,
+    "location_type": 0,
+    "stop_code": ""
+  },
+
+route of route_id:
+  {
+    "agency_id": 2,
+    "route_id": 1,
+    "route_short_name": "1",
+    "route_long_name": "Str. Bucium - P-ta 1 Mai",
+    "route_color": "#f3513c",
+    "route_type": 11,
+    "route_desc": "Str. Bucium - P-ta 1 Mai"
+  },
+
+shapes of shape_id:
+  [
+    {
+      "shape_id": "1_0",
+      "shape_pt_lat": 46.75123,
+      "shape_pt_lon": 23.54317,
+      "shape_pt_sequence": 0
+    },
+    {
+      "shape_id": "1_0",
+      "shape_pt_lat": 46.75125,
+      "shape_pt_lon": 23.5432,
+      "shape_pt_sequence": 1
+    },
+    {
+      "shape_id": "1_0",
+      "shape_pt_lat": 46.75129,
+      "shape_pt_lon": 23.54327,
+      "shape_pt_sequence": 2
+    },
+    ...
+  ]
+*/
